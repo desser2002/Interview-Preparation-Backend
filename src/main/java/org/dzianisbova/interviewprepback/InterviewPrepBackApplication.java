43d6fdf470e8974323fcc9668aa9fafad44fcf8d
bovadenis <bovadzianis@gmail.con>
2025-11-16 17:15:08 +0100

    initialize Spring Boot application with Maven setup and basic configuration

---
+++ src/main/java/org/dzianisbova/interviewprepback/InterviewPrepBackApplication.java
@@ -0,0 +1,13 @@
+package org.dzianisbova.interviewprepback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewPrepBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewPrepBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/dzianisbova/interviewprepback/InterviewPrepBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dzianisbova.interviewprepback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewPrepBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
